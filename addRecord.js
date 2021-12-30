68530f923f1c4bd209d46ffda1b092cb0214ba71
aremumahmud <74737123+aremumahmud@users.noreply.github.com>
2021-12-30 16:33:43 +0100

    Add files via upload

---
+++ addRecord.js
@@ -0,0 +1,31 @@
+var FileRecord = require("./ServerFileRecord.Model.js")
+module.exports= function(req, res){
+var {
+    serverId,
+    FileFolderPath,
+    FileRoutePath
+  } = req.query
+  var fileRecord = new FileRecord()
+ //-------test----
+ console.log(req.query)
+  ///var fileRecord = FileRecord
+  //------end-------
+  fileRecord.serverId = serverId
+  fileRecord.FileRoutePath = FileRoutePath
+  fileRecord.FileFolderPath = FileFolderPath
+  fileRecord.save((err, record)=>{
+    if (err){
+      res.status(404)
+      res.json({
+        "error" : true,
+        "msg" : "network filesave err"
+      })
+    }else {
+      res.status(200)
+      res.send(JSON.stringify({
+           error : false,
+           record 
+        }))
+    }
+  })
+}
