563ff1b42040777d8f656cce1f7f55adad53d5e4
aremumahmud <74737123+aremumahmud@users.noreply.github.com>
2021-12-30 16:20:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,28 @@
-//------end-------
-var fs = require("fs")
+
 var express = require("express")
 var mongoose =require("mongoose")
+/*
 var updateRecord = require("./updateRecord")
 var addRecord = require("./addRecord")
 var getRecord = require("./getRecord")
 var getAllRecords = require("./getAllRecord")
 var deleteRecord = require("./deleteRecord")
 
-
+*/
 
 var app = express()
 app.listen(process.env.PORT || 2000)
 
-
-
-var txt = fs.readFileSync("mongodb.txt").toString().split('\n')
-var password = txt[0].split(" ")[1]
-var username = txt[1].split(" ")[1]
-var database = txt[2].split(" ")[1]
-
+var appl;
 var uri = `mongodb://FileuploadService:aremu2003@cluster0-shard-00-00.brdt8.mongodb.net:27017,cluster0-shard-00-01.brdt8.mongodb.net:27017,cluster0-shard-00-02.brdt8.mongodb.net:27017/totalcare?ssl=true&replicaSet=atlas-cbfjj2-shard-0&authSource=admin&retryWrites=true&w=majority`
 mongoose.connect(uri)
     .then(res=>{
       console.log(res ,"connected")
+       appl=1
    })
    .catch(err=>{console.log(err)})
    
-   
+   /*
    app.get("/updateRecord", (req, res)=>{
      //res.send("hi")
      updateRecord(req,res)
@@ -44,4 +39,5 @@ mongoose.connect(uri)
    app.get("/deleteRecord",(req, res)=>{
    //  res.send("hi")
      deleteRecord(req, res)
-   })
+   })*/
+app.get("/",(req,res)=>{res.send(appl)})
